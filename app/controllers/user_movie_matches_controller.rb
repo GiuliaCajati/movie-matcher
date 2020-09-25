fc5edfd4e985a145dc466725f11b033b7f67a1ba
giuliacajati <giuliacajati@gmail.com>
2020-09-25 15:50:50 -0400

    User update path fixed, added user movie matches create page.

--- app/controllers/user_movie_matches_controller.rb
+++ app/controllers/user_movie_matches_controller.rb
@@ -1,2 +1,17 @@
 class UserMovieMatchesController < ApplicationController
+
+    def new 
+        @usermoviematch = UserMovieMatch.new 
+    end 
+    
+    def create 
+        usermoviematch = UserMovieMatch.create(matches_params)
+        redirect_to user_path(@user)
+    end 
+
+    private 
+
+    def matches_params 
+        params.require(:usermoviematch).permit(:user_id, :movie_id)
+    end 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,11 +23,9 @@ class UsersController < ApplicationController
 
   def update 
     @user = User.find(params[:id])
-    @user = User.update(user_params)
+    @user.update(user_params)
     redirect_to user_path(@user)
   end 
-
-  
 
   private 
 
