ef4043ad7bcb13a2cbbe3f466f4615d9d1ccfd57
Melindadiaz07 <melindadiaz@me.com>
2020-09-29 15:03:23 -0400

    Added password column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_25_180544) do
+ActiveRecord::Schema.define(version: 2020_09_29_185954) do
 
   create_table "cities", force: :cascade do |t|
     t.string "name"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2020_09_25_180544) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "city_id"
+    t.string "password_digest"
   end
 
 end
